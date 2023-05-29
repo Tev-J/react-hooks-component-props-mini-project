0d1ad9a88b8a025a97710285b623569e4f2a4560
Tev-J <tj_delph@yahoo.com>
2023-05-29 03:22:33 -0400

    complete lab

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -5,11 +5,18 @@ function ArticleList({ posts }) {
   return (
     <main>
       {posts.map((post) => {
+        const emoji = post.minutes < 30 ? "☕️" : "🍱";
+        const repeat =
+          post.minutes < 30
+            ? Math.ceil(post.minutes / 5)
+            : Math.ceil(post.minutes / 10);
         return (
           <Article
             key={post.id}
             title={post.title}
-            date={post.date}
+            date={`${post.date}  • ${emoji.repeat(repeat)} ${
+              post.minutes
+            } minutes `}
             preview={post.preview}
           />
         );
